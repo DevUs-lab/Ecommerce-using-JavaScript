8c44ecd9ce9af4422bd08bfaade4b26e3a93597f
Umair Saeed <umairdevus@gmail.com>
2026-01-12 18:58:20 +0500

    instead image else product uploding functions is working

--- src/pages/Frontend/Admin/Products.jsx
+++ src/pages/Frontend/Admin/Products.jsx
@@ -1,36 +1,77 @@
-import React from 'react'
+import { message } from 'antd'
+import { collection, addDoc } from "firebase/firestore";
+import React, { useState } from 'react'
+import { db } from '../../../firebase/config';
 
 const Products = () => {
+    const initialstate = {
+        productName: '',
+        descriptions: '',
+        sellPrice: '',
+        delPrice: '',
+        stock: ''
+    }
+
+    const [loading, setLoading] = useState(false)
+    const [products, setProducts] = useState(initialstate)
+    const handleChange = (e) => {
+        setProducts({ ...products, [e.target.name]: e.target.value })
+    }
+
+
+    const HandleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            setLoading(true)
+
+            const docRef = await addDoc(collection(db, "users"), {
+                ...products, createdAt: new Date()
+            });
+            console.log("Document written with ID: ", docRef.id);
+            message.success("Product added successfully");
+
+        } catch (error) {
+            console.log('error', error)
+            message.error('Failed to add product')
+        }
+        setLoading(false)
+
+    }
+
+
+
     return (
         <div className='container'>
             <div className="row">
                 <div className="col">
 
                     <h2 className='text-center py-5'>Add Product</h2>
-                    <form action="">
+                    <form onSubmit={HandleSubmit}>
 
                         <div>
-                            <input type="file" className='form-control' name="" id="" />
+                            <input type="file" className='form-control' name="image-url" id="" />
                         </div>
 
                         <div className='my-3'>
-                            <input type="text" placeholder='Product Name' className='form-control' name='productName' />
+                            <input type="text" placeholder='Product Name' className='form-control' onChange={handleChange} name='productName' />
                         </div>
                         <div className='my-3'>
-                            <textarea type="text" placeholder='Product description' className='form-control' name='descriptions' />
+                            <textarea type="text" placeholder='Product description' className='form-control' onChange={handleChange} name='descriptions' />
                         </div>
                         <div className='my-3'>
-                            <input type="number" placeholder='Sell Price' className='form-control' name='sellPrice' />
+                            <input type="number" placeholder='Sell Price' className='form-control' name='sellPrice' onChange={handleChange} />
                         </div>
                         <div className='my-3'>
-                            <input type="number" placeholder='Del Price' className='form-control' name='delPrice' />
+                            <input type="number" placeholder='Del Price' className='form-control' name='delPrice' onChange={handleChange} />
                         </div>
                         <div className='my-3'>
-                            <input type="number" placeholder='stock' className='form-control' name='stock' />
+                            <input type="number" placeholder='stock' className='form-control' name='stock' onChange={handleChange} />
                         </div>
                         <div className='text-center'>
-
-                            <button className='btn btn-primary w-50 text-center'>Add Product</button>
+                            {
+                                loading ? <button className='btn btn-primary w-50 text-center' disabled>Adding...</button> : <button className='btn btn-primary w-50 text-center'>Add Product</button>
+                            }
                         </div>
                     </form>
                 </div>
--- src/pages/Frontend/Admin/index.jsx
+++ src/pages/Frontend/Admin/index.jsx
@@ -7,11 +7,15 @@ import Products from "./Products"
 import Settings from "./Settings"
 
 export default function AppRouter() {
+
+
+
+
     return (
         <>
             <div className=".container">
                 <div className="row">
-                    <div className="col-4 d-flex min-vh-100  bg-dark flex-column text-white">
+                    <div className="col-3 col-md-4 d-flex bg-dark flex-column text-white">
 
                         <h3 className="mt-5 text-center">Admin Panel</h3>
                         <Link to="/admin" className="py-3 ms-4 text-decoration-none hover:dark">Dashboard</Link>
@@ -20,7 +24,7 @@ export default function AppRouter() {
                         <Link to="/admin/settings" className="py-3 text-decoration-none ms-4 hover:dark">Setting</Link>
 
                     </div>
-                    <div className="col-8">
+                    <div className="col-9 col-md-8 p-4">
                         <Routes>
                             <Route path="/" element={<Dashboard />} />
                             <Route path="/users" element={<Users />} />
